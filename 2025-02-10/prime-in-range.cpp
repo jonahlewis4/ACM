dfa3682cfc7f88c1c68f6bc54f4beba9215607de
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-10 18:08:49 -0800

    prime range

---
+++ 2025-02-10/prime-in-range.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Jonah on 2/10/25.
+//
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+int n = 1000;
+int square_root_n = std::floor(std::sqrt(n));
+int main(){
+    std::vector<bool> primes(n + 1, true);
+    for(int i = 2; i <= square_root_n; i++){
+        if(!primes[i])
+        {
+            continue;
+        }
+        for(int j = 2 * i; j <= n; j += i){
+            primes[j] = false;
+        }
+    }
+
+
+
+
+    //postprocessing
+    std::vector<int> primesV;
+    for(int i = 2; i <= n; i++){
+      if(primes[i]){
+          primesV.push_back(i);
+      }
+    }
+
+    for(const int prime : primesV){
+        std::cout<<prime<<" ";
+    }
+    std::cout<<std::endl;
+}
